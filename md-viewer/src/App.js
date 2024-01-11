b1023353be2886f92c0b4f195e986617cc063a63
Joe McGeoch Osborne <joe@joes.games>
2024-01-11 11:51:48 +0100

    Add placeholder components for md viewer

--- md-viewer/src/App.js
+++ md-viewer/src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MarkdownInput() {
+  return (
+    <form>
+      <input></input>
+    </form>
+  );
+}
+
+function MarkdownViewer() {
+  return (
+    <div>Placeholder</div>
+  );
+}
+
+// TODO: Support file upload / import?
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to.
         </p>
         <a
           className="App-link"
@@ -18,6 +34,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <MarkdownInput />
+        <MarkdownViewer />
+      </main>
     </div>
   );
 }
