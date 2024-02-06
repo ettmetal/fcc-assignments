6cf108fb5da805c73ac6a2b0f29918ab79d60c78
Joe McGeoch Osborne <joe@joes.games>
2024-02-06 12:33:01 +0100

    Pass initial markdown test

--- md-viewer/src/App.js
+++ md-viewer/src/App.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 // TODO: Supoprt file print / to PDF or rendered?
 
 function App() {
-  const [markdown, setMarkdown] = useState('# Hello world!\nI am some demo markdown.');
+  const [markdown, setMarkdown] = useState('# Hello world!\nI am some demo markdown.\n\n## This is a sub-heading\n\n**Check out this list:**\n\n- First item\n- second item\n\nMarkdown is handy for writing `code`, like so:\n\n```html\n<!doctype HTML>\n<html>\n\t<head>\n\t\t<title>Demo HTML Page</title>\n\t</head>\n\t<body>\n\t\t<p>\n\t\t\tHello, world!\n\t\t</p>\n\t</body>\n</html>\n```\n\nIt can create block-quotes, too:\n\n> For reference or callout purposes\n\nAnd even load images.\n\n![freeCodeCamp](https://design-style-guide.freecodecamp.org/downloads/fcc_primary_large.png)');
   return (
     <>
       <header className="App-header">
--- md-viewer/src/MarkdownInput.js
+++ md-viewer/src/MarkdownInput.js
@@ -13,7 +13,7 @@ function MarkdownInput(props) {
 }
 
 MarkdownInput.defaultProps = {
-    markdown: '# Hello world!\nI am some demo markdown.'
+    markdown: '# Hello world!\nI am some demo markdown. [This is how it works](https://www.markdownguide.org/).\n\n## This is a sub-heading\n\n**Check out this list:**\n\n- First item\n- second item\n\nMarkdown is handy for writing `code`, like so:\n\n```html\n<!doctype HTML>\n<html>\n\t<head>\n\t\t<title>Demo HTML Page</title>\n\t</head>\n\t<body>\n\t\t<p>\n\t\t\tHello, world!\n\t\t</p>\n\t</body>\n</html>\n```\n\nIt can create block-quotes, too:\n\n> For reference or callout purposes\n\nAnd even load images.\n\n![freeCodeCamp](https://design-style-guide.freecodecamp.org/downloads/fcc_primary_large.png)'
 };
 
 MarkdownInput.propTypes = {
