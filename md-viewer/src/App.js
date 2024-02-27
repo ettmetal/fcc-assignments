5d5cae1d330a6a52323ca1c0505bc9efec332b94
Joe McGeoch Osborne <joe@joes.games>
2024-02-27 12:08:25 +0100

    Add support for opening md files

--- md-viewer/src/App.js
+++ md-viewer/src/App.js
@@ -4,9 +4,10 @@ import MarkdownInput from './MarkdownInput';
 import MarkdownViewer from './MarkdownViewer';
 import { useState } from 'react';
 
-// TODO: Support file upload / import?
 // TODO: Support file export
 // TODO: Supoprt file print / to PDF or rendered?
+
+// TODO: Style better -> file buttons to the right side? (or to flex-start for rtl)
 
 function App() {
   const [markdown, setMarkdown] = useState('# Hello world!\nI am some demo markdown. [This is how it works](https://www.markdownguide.org/).\n\n## This is a sub-heading\n\n**Check out this list:**\n\n- First item\n- second item\n\nMarkdown is handy for writing `code`, like so:\n\n```html\n<!doctype HTML>\n<html>\n\t<head>\n\t\t<title>Demo HTML Page</title>\n\t</head>\n\t<body>\n\t\t<p>\n\t\t\tHello, world!\n\t\t</p>\n\t</body>\n</html>\n```\n\nIt can create block-quotes, too:\n\n> For reference or callout purposes\n\nAnd even load images.\n\n![freeCodeCamp](https://design-style-guide.freecodecamp.org/downloads/fcc_primary_large.png)');
--- md-viewer/src/MarkdownInput.js
+++ md-viewer/src/MarkdownInput.js
@@ -4,9 +4,17 @@ import PropTypes from 'prop-types';
 function MarkdownInput(props) {
     const updateInput = event => props.markdownSetter(event.target.value);
     const clearInput = () => props.markdownSetter("");
+    const loadFile = event => {
+        event.preventDefault();
+        const reader = new FileReader();
+        reader.onload = event => props.markdownSetter(event.target.result);
+        reader.readAsText(event.target.files[0]);
+    }
     return (
         <>
             <label htmlFor="editor">Type markdown:</label>
+            <label htmlFor='fileOpen'>Open a file</label>
+            <input id="fileOpen" type="file" accept=".md,text/markdown" onChange={loadFile}></input>
             <button title="Clear input" aria-label="Clear input" onClick={clearInput}><i class="fa fa-solid fa-rotate" aria-hidden="true" /></button>
             <textarea type="text" id="editor" onChange={updateInput} rows="30" defaultValue={props.markdown} value={props.markdown}></textarea>
         </>
