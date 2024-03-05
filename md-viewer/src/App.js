bc3e1bdf7607ba9bd9b3575f93ee39e63c3319b1
Joe McGeoch Osborne <joe@joes.games>
2024-03-05 11:59:35 +0100

    Add a custom pages workflow

--- md-viewer/src/App.js
+++ md-viewer/src/App.js
@@ -9,8 +9,6 @@ import { useState } from 'react';
 
 // TODO: Style better -> file buttons to the right side? (or to flex-start for rtl)
 
-// TODO: GH pages build to get it online better 
-
 function App() {
   const [markdown, setMarkdown] = useState('# Hello world!\nI am some demo markdown. [This is how it works](https://www.markdownguide.org/).\n\n## This is a sub-heading\n\n**Check out this list:**\n\n- First item\n- second item\n\nMarkdown is handy for writing `code`, like so:\n\n```html\n<!doctype HTML>\n<html>\n\t<head>\n\t\t<title>Demo HTML Page</title>\n\t</head>\n\t<body>\n\t\t<p>\n\t\t\tHello, world!\n\t\t</p>\n\t</body>\n</html>\n```\n\nIt can create block-quotes, too:\n\n> For reference or callout purposes\n\nAnd even load images.\n\n![freeCodeCamp](https://design-style-guide.freecodecamp.org/downloads/fcc_primary_large.png)');
   return (
