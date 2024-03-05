fe59e1f99529634b65854e24d7407fb2c7da1f3d
Joe McGeoch Osborne <joe@joes.games>
2024-03-05 11:07:07 +0100

    Add file export

--- md-viewer/src/App.js
+++ md-viewer/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import MarkdownInput from './MarkdownInput';
 import MarkdownViewer from './MarkdownViewer';
+import MarkdownExporter from './MarkdownExporter';
 import { useState } from 'react';
 
-// TODO: Support file export
 // TODO: Supoprt file print / to PDF or rendered?
 
 // TODO: Style better -> file buttons to the right side? (or to flex-start for rtl)
+
+// TODO: GH pages build to get it online better 
 
 function App() {
   const [markdown, setMarkdown] = useState('# Hello world!\nI am some demo markdown. [This is how it works](https://www.markdownguide.org/).\n\n## This is a sub-heading\n\n**Check out this list:**\n\n- First item\n- second item\n\nMarkdown is handy for writing `code`, like so:\n\n```html\n<!doctype HTML>\n<html>\n\t<head>\n\t\t<title>Demo HTML Page</title>\n\t</head>\n\t<body>\n\t\t<p>\n\t\t\tHello, world!\n\t\t</p>\n\t</body>\n</html>\n```\n\nIt can create block-quotes, too:\n\n> For reference or callout purposes\n\nAnd even load images.\n\n![freeCodeCamp](https://design-style-guide.freecodecamp.org/downloads/fcc_primary_large.png)');
@@ -18,6 +20,7 @@ function App() {
       </header>
       <main>
         <MarkdownInput markdownSetter={setMarkdown} markdown={markdown} />
+        <MarkdownExporter markdown={markdown} />
         <MarkdownViewer markdown={markdown} />
       </main>
     </>
---
+++ md-viewer/src/MarkdownExporter.js
@@ -0,0 +1,17 @@
+import FileSaver, { saveAs } from 'file-saver';
+
+function MarkdownExporter(props) {
+    const save = () => {
+        const blob = new Blob([props.markdown], {type:"text/markdown;charset=utf-8"});
+        FileSaver.saveAs(blob, "markdown.md");
+    }
+    return (
+        <>
+            <button title="Save markdown" aria-label="Save markdown" onClick={save}>
+                <i class="fa fa-solid fa-floppy-disk" aria-hidden="true" />
+            </button>
+        </>
+    );
+}
+
+export default MarkdownExporter;
