3cf67f7462ab1f3b2cb11e232c79234cc6368d57
Joe McGeoch Osborne <joe@joes.games>
2024-03-20 15:06:32 +0100

    Add drum machine project

---
+++ drum-machine/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import DrumMachine from './DrumMachine';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Drum Machine</h1>
+      </header>
+      <main id="drum-machine">
+        <DrumMachine />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ drum-machine/src/DrumMachine.js
@@ -0,0 +1,5 @@
+function DrumMachine(props) {
+
+}
+
+export default DrumMachine;
