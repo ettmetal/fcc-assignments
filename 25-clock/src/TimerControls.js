40b86879db10a46b24c779ca965d9232712a0fd0
Joe McGeoch Osborne <joe@joes.games>
2024-03-25 11:59:28 +0100

    Add styling for controls

--- 25-clock/src/TimerControls.js
+++ 25-clock/src/TimerControls.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import './TimerControl.css';
 
 function TimerControls(){
     const running = useSelector(state => state.clock.value.running);
@@ -6,17 +7,23 @@ function TimerControls(){
     let startStopLabel = running ? "Stop the timer" : "Start the timer";
 
     return (
-        <>
-            <button id="start_stop">
-                <i className={`fa-solid ${startStopIcon}`} title={startStopLabel}></i>
-            </button>
-            <button>
-                <i className="fa-solid fa-pause"></i>
-            </button>
-            <button id="reset">
-                <i className="fa-solid fa-arrows-rotate"></i>
-            </button>
-        </>
+        <menu>
+            <li>
+                <button id="start_stop">
+                    <i className={`fa-solid ${startStopIcon}`} title={startStopLabel}></i>
+                </button>
+            </li>
+            <li>
+                <button>
+                    <i className="fa-solid fa-pause"></i>
+                </button>
+            </li>
+            <li>
+                <button id="reset">
+                    <i className="fa-solid fa-arrows-rotate"></i>
+                </button>
+            </li>
+        </menu>
     );
 }
 
