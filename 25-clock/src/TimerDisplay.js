d424852b8f35610c04cbbabd47230114f00cd79e
Joe McGeoch Osborne <joe@joes.games>
2024-03-25 10:35:17 +0100

    Sloppy time formatting pass

--- 25-clock/src/TimerDisplay.js
+++ 25-clock/src/TimerDisplay.js
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux";
 
 function TimerDisplay() {
+    const length = useSelector(state => state.clock.value[`${state.clock.value.phase.toLowerCase()}Length`]);
+    const timeSinceStart = useSelector(state => state.clock.value.timeSinceStart)
+    const remaining = length - timeSinceStart;
     return(
-        <output id="time-left">
+        <>
             <header>
                 <h2 id="timer-label">{useSelector(state => state.clock.value.phase)}</h2>
             </header>
-            {useSelector(state => state.clock.value[`${state.clock.value.phase.toLowerCase()}Length`] - state.clock.value.timeSinceStart)}
-        </output>
+            <output id="time-left">
+                {`${remaining < 10 ? "0": ""}${remaining}:00`}
+            </output>
+        </>
     );
 }
 
