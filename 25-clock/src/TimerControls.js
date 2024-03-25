0494439e3665a64d269e533297cc1e1506a9f956
Joe McGeoch Osborne <joe@joes.games>
2024-03-25 13:01:54 +0100

    Implement control buttons

--- 25-clock/src/TimerControls.js
+++ 25-clock/src/TimerControls.js
@@ -1,4 +1,5 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { pause, reset, start, stop } from "./clockSlice";
 import './TimerControl.css';
 
 function TimerControls(){
@@ -6,20 +7,23 @@ function TimerControls(){
     let startStopIcon = running ? "fa-stop" : "fa-play";
     let startStopLabel = running ? "Stop the timer" : "Start the timer";
 
+    const dispatch = useDispatch();
+    const startStopAction = useSelector(state => state.clock.value.running) ? start : stop;
+
     return (
         <menu>
             <li>
-                <button id="start_stop">
+                <button id="start_stop" onClick={() => dispatch(startStopAction())}>
                     <i className={`fa-solid ${startStopIcon}`} title={startStopLabel}></i>
                 </button>
             </li>
             <li>
-                <button>
+                <button onClick={() => dispatch(pause())}>
                     <i className="fa-solid fa-pause"></i>
                 </button>
             </li>
             <li>
-                <button id="reset">
+                <button id="reset" onClick={() => dispatch(reset())}>
                     <i className="fa-solid fa-arrows-rotate"></i>
                 </button>
             </li>
